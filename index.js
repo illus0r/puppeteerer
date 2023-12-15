d214a2c3d017a6e8d8827e2aa26e95c87b15867b
illus0r <zor667@gmail.com>
2023-12-15 19:17:34 +0300

    Added params

--- index.js
+++ index.js
@@ -1,58 +1,74 @@
 const puppeteer = require('puppeteer');
 const { createCanvas, loadImage } = require('canvas');
 const fs = require('fs');
+const yargs = require('yargs');
+
+const argv = yargs
+  .option('maxsize', {
+    describe: 'Maximum collage size',
+    type: 'number',
+    default: 4000,
+  })
+  .option('timeout', {
+    describe: 'Page wait timeout',
+    type: 'number',
+    default: 100,
+  })
+  .argv;
 
 (async () => {
-	const browser = await puppeteer.launch({ headless: false });
-	let rndStr = Math.random().toString(36).substring(2, 15);
-	let dir = `/Users/ivandianov/Downloads/${rndStr}`
-	let size = [1024, 1657]
-	let num = 128
-	let cols = Math.floor(Math.sqrt(num))
-	let rows = Math.ceil(num / cols)
-	let maxCollageSize = 4000
-	let collageScale = Math.min(maxCollageSize / (cols * size[0]), maxCollageSize / (rows * size[1]))
-	let miniatureWidth = size[0] * collageScale
-	let miniatureHeight = size[1] * collageScale
-	const url = `http://127.0.0.1:8080`;
-	fs.mkdirSync(dir);
+  const args = process.argv.slice(2);
+  const url = args[0] || 'http://127.0.0.1:8080';
+  const width = parseInt(args[1]) || 512;
+  const height = parseInt(args[2]) || width;
+  const cols = parseInt(args[3]) || 4;
+  const rows = parseInt(args[4]) || 4;
 
-	let screenshots = []
-	const page = await browser.newPage();
-	await page.setViewport({ width: size[0], height: size[1] });
-  for (let i=0; i<num; i++) {
-		console.log(i)
+  const browser = await puppeteer.launch({ headless: false });
+  let rndStr = Math.random().toString(36).substring(2, 15);
+  let dir = `/Users/ivandianov/Downloads/${rndStr}`;
+  let size = [width, height];
+  let num = cols * rows;
+  let maxCollageSize = argv.maxsize;
+  let collageScale = Math.min(maxCollageSize / (cols * size[0]), maxCollageSize / (rows * size[1]));
+  let miniatureWidth = size[0] * collageScale;
+  let miniatureHeight = size[1] * collageScale;
+
+  fs.mkdirSync(dir);
+
+  let screenshots = [];
+  const page = await browser.newPage();
+  await page.setViewport({ width: size[0], height: size[1] });
+  for (let i = 0; i < num; i++) {
+    console.log(i);
     await page.goto(url);
-    await page.waitForTimeout(100);
+    await page.waitForTimeout(argv.timeout);
     let screenshot = await page.screenshot({ path: `${dir}/${i}.png` });
-		screenshots.push(screenshot)
+    screenshots.push(screenshot);
   }
-	await page.close();
+  await page.close();
   await browser.close();
 
-	async function createCollage(screenshots) {
+  async function createCollage(screenshots) {
+    const canvas = createCanvas(miniatureWidth * cols, miniatureHeight * rows);
+    const ctx = canvas.getContext('2d');
 
+    let row = 0;
+    let col = 0;
+    for (const screenshot of screenshots) {
+      const img = await loadImage(screenshot);
+      ctx.drawImage(img, col * miniatureWidth, row * miniatureHeight, miniatureWidth, miniatureHeight);
 
-		const canvas = createCanvas(miniatureWidth*cols, miniatureHeight*rows);
-		const ctx = canvas.getContext('2d');
+      col++;
+      if (col === cols) {
+        col = 0;
+        row++;
+      }
+    }
+    const collageBuffer = canvas.toBuffer('image/png');
+    fs.writeFileSync(`${dir}/collage.png`, collageBuffer);
+  }
 
-		let row = 0;
-		let col = 0;
-		for (const screenshot of screenshots) {
-			const img = await loadImage(screenshot);
-			ctx.drawImage(img, col * miniatureWidth, row * miniatureHeight, miniatureWidth, miniatureHeight);
-
-			col++;
-			if (col === cols) {
-				col = 0;
-				row++;
-			}
-		}
-		const collageBuffer = canvas.toBuffer('image/png');
-		fs.writeFileSync(`${dir}/collage.png`, collageBuffer);
-	}
-
-	await createCollage(screenshots)
-
+  await createCollage(screenshots);
 })();
 
